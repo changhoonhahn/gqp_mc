dc82c324dfaf624e2d9ac67b54f9f82a7f86e41f
changhoonhahn <changhoonhahn@lbl.gov>
2019-10-01 12:54:15 -0700

    cleaning up the code to remove outdated spectral fitting code

--- run/make_data.py
+++
@@ -1,9 +0,0 @@
-from gqp_mc import data as Data 
-
-# compile Lgal Photometry data for Spectral Challenge
-#Data._make_Lgal_Photometry_SpectralChallenge() 
-#Data._make_Lgal_Photometry() 
-
-# compile Lgal Spectra data for Spectral Challenge
-Data._make_Lgal_Spectra_SpectralChallenge() 
-#Data._make_Lgal_Spectra() 

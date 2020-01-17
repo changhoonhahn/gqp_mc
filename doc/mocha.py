9d934e43c3f6bafffd9b4d058166e2f4052606cc
changhoonhahn <changhoonhahn@lbl.gov>
2020-01-17 15:25:57 -0800

    - comparison of mstar inferred from photo vs mstar inferred from specphoto shows that spectra adds significant info even when you marginalize

--- doc/mocha.py
+++ doc/mocha.py
@@ -168,6 +168,76 @@ def mini_mocha_spec(noise='bgs0', method='ifsps'):
     return None 
 
 
+def mini_mocha_photo(noise='legacy', method='ifsps'): 
+    ''' Compare properties inferred from forward modeled photometry to input properties
+    '''
+    # read noiseless Lgal spectra of the spectral_challenge mocks
+    photo, meta = Data.Photometry(sim='lgal', noise=noise, lib='bc03', sample='mini_mocha')
+
+    Mstar_input = meta['logM_total'][:97] # total mass 
+    Z_MW_input  = meta['Z_MW'][:97]  # mass-weighted metallicity
+    tage_input  = meta['t_age_MW'][:97]  # mass-weighted age
+    
+    theta_inf = [] 
+    for igal in range(97): 
+        # read best-fit file and get inferred parameters
+        _fbf = Fbestfit_photo(igal, noise=noise, method=method) 
+        fbf = h5py.File(_fbf, 'r')  
+
+        theta_inf_i = np.array([
+            fbf['theta_2sig_minus'][...], 
+            fbf['theta_1sig_minus'][...], 
+            fbf['theta_med'][...], 
+            fbf['theta_1sig_plus'][...], 
+            fbf['theta_2sig_plus'][...]])
+        theta_inf.append(theta_inf_i) 
+    theta_inf = np.array(theta_inf) 
+    
+    # inferred properties
+    Mstar_inf   = theta_inf[:,:,0]
+    Z_MW_inf    = 10**theta_inf[:,:,1]
+    tage_inf    = theta_inf[:,:,2]
+    
+    fig = plt.figure(figsize=(15,4))
+    # compare total stellar mass 
+    sub = fig.add_subplot(131) 
+    sub.errorbar(Mstar_input, Mstar_inf[:,2], 
+            yerr=[Mstar_inf[:,2]-Mstar_inf[:,1], Mstar_inf[:,3]-Mstar_inf[:,2]], fmt='.C0')
+    sub.plot([9., 12.], [9., 12.], c='k', ls='--') 
+    sub.set_xlabel(r'input $\log~M_{\rm tot.}$', fontsize=25)
+    sub.set_xlim(9., 12.) 
+    sub.set_ylabel(r'inferred $\log~M_{\rm tot.}$', fontsize=25)
+    sub.set_ylim(9., 12.) 
+    
+    # compare metallicity
+    sub = fig.add_subplot(132)
+    sub.errorbar(Z_MW_input, Z_MW_inf[:,2], 
+            yerr=[Z_MW_inf[:,2]-Z_MW_inf[:,1], Z_MW_inf[:,3]-Z_MW_inf[:,2]], fmt='.C0')
+    sub.plot([1e-3, 1], [1e-3, 1.], c='k', ls='--') 
+    sub.set_xlabel(r'input MW $Z$', fontsize=20)
+    sub.set_xscale('log') 
+    sub.set_xlim(1e-3, 5e-2) 
+    sub.set_ylabel(r'inferred MW $Z$', fontsize=20)
+    sub.set_yscale('log') 
+    sub.set_ylim(1e-3, 5e-2) 
+
+    # compare age 
+    sub = fig.add_subplot(133)
+    sub.errorbar(tage_input, tage_inf[:,2], 
+            yerr=[tage_inf[:,2]-tage_inf[:,1], tage_inf[:,3]-tage_inf[:,2]], fmt='.C0')
+    sub.plot([0, 13], [0, 13.], c='k', ls='--') 
+    sub.set_xlabel(r'input MW $t_{\rm age}$', fontsize=20)
+    sub.set_xlim(0, 13) 
+    sub.set_ylabel(r'inferred MW $t_{\rm age}$', fontsize=20)
+    sub.set_ylim(0, 13) 
+
+    fig.subplots_adjust(wspace=0.4)
+    _ffig = os.path.join(dir_fig, 'mini_mocha.%s.photofit.vanilla.noise_%s.png' % (method, noise)) 
+    fig.savefig(_ffig, bbox_inches='tight') 
+    fig.savefig(UT.fig_tex(_ffig, pdf=True), bbox_inches='tight') 
+    return None 
+
+
 def mini_mocha_specphoto(noise='bgs0_legacy', method='ifsps'): 
     ''' Compare properties inferred from forward modeled photometry to input properties
     '''
@@ -246,6 +316,104 @@ def mini_mocha_specphoto(noise='bgs0_legacy', method='ifsps'):
     return None 
 
 
+def photo_vs_specphoto(noise_photo='legacy', noise_specphoto='bgs0_legacy', method='ifsps'):  
+    ''' Compare properties inferred from photometry versus spectrophotometry to see how much
+    information is gained from adding spectra
+    '''
+    assert noise_specphoto.split('_')[1] == noise_photo
+    # read noiseless Lgal spectra of the spectral_challenge mocks
+    specs, meta = Data.Spectra(sim='lgal', noise=noise_specphoto.split('_')[0], lib='bc03', sample='mini_mocha') 
+    # read Lgal photometry of the mini_mocha mocks
+    photo, _ = Data.Photometry(sim='lgal', noise=noise_specphoto.split('_')[1], lib='bc03', sample='mini_mocha')
+
+    Mstar_input = np.array(meta['logM_total'][:97]) # total mass 
+    Z_MW_input  = meta['Z_MW'][:97]  # mass-weighted metallicity
+    tage_input  = meta['t_age_MW'][:97]  # mass-weighted age
+    
+    theta_inf_photo, theta_inf_specphoto = [], []
+    for igal in range(97): 
+        # read best-fit file and get inferred parameters from photometry
+        _fbf = Fbestfit_photo(igal, noise=noise_photo, method=method) 
+        fbf = h5py.File(_fbf, 'r')  
+
+        theta_inf_i = np.array([
+            fbf['theta_2sig_minus'][...], 
+            fbf['theta_1sig_minus'][...], 
+            fbf['theta_med'][...], 
+            fbf['theta_1sig_plus'][...], 
+            fbf['theta_2sig_plus'][...]])
+        theta_inf_photo.append(theta_inf_i) 
+        # read best-fit file and get inferred parameters from spectrophoto
+        _fbf = Fbestfit_specphoto(igal, noise=noise_specphoto, method=method) 
+        fbf = h5py.File(_fbf, 'r')  
+
+        theta_inf_i = np.array([
+            fbf['theta_2sig_minus'][...], 
+            fbf['theta_1sig_minus'][...], 
+            fbf['theta_med'][...], 
+            fbf['theta_1sig_plus'][...], 
+            fbf['theta_2sig_plus'][...]])
+        theta_inf_specphoto.append(theta_inf_i) 
+    theta_inf_photo = np.array(theta_inf_photo) 
+    theta_inf_specphoto = np.array(theta_inf_specphoto) 
+    
+    # inferred properties
+    Mstar_inf_photo = theta_inf_photo[:,:,0]
+    Mstar_inf_specphoto = theta_inf_specphoto[:,:,0]
+
+    Mbins = np.linspace(9., 12., 16) 
+    Mbin_mid = [] 
+    dMstar_photo, sig_dMstar_photo = [], []
+    dMstar_specphoto, sig_dMstar_specphoto = [], [] 
+    for i_m in range(len(Mbins)-1): 
+        inmbin = ((Mstar_input > Mbins[i_m]) & (Mstar_input <= Mbins[i_m+1])) 
+        if np.sum(inmbin) == 0: continue 
+        Mbin_mid.append(0.5 * (Mbins[i_m] + Mbins[i_m+1])) 
+
+        dMstar_photo.append(np.average(Mstar_inf_photo[:,2][inmbin] - Mstar_input[inmbin])) 
+        dMstar_specphoto.append(np.average(Mstar_inf_specphoto[:,2][inmbin] - Mstar_input[inmbin])) 
+
+        sig_dMstar_photo.append(
+                np.sqrt(np.sum(
+                    np.max([Mstar_input[inmbin] - Mstar_inf_photo[inmbin,1], 
+                        Mstar_inf_photo[inmbin,3] - Mstar_input[inmbin]], axis=1)
+                    ))/np.float(np.sum(inmbin)))
+        sig_dMstar_specphoto.append(
+                np.sqrt(np.sum(
+                    np.max([Mstar_input[inmbin] - Mstar_inf_specphoto[inmbin,1], 
+                        Mstar_inf_specphoto[inmbin,3] - Mstar_input[inmbin]], axis=1)
+                    ))/np.float(np.sum(inmbin)))
+    dMstar_photo = np.array(dMstar_photo) 
+    sig_dMstar_photo = np.array(sig_dMstar_photo) 
+    dMstar_specphoto = np.array(dMstar_specphoto) 
+    sig_dMstar_specphoto = np.array(sig_dMstar_specphoto) 
+
+    fig = plt.figure(figsize=(5,5))
+    # compare total stellar mass 
+    sub = fig.add_subplot(111) 
+    sub.plot([9., 12.], [0., 0.], c='k', ls='--')
+    sub.fill_between(Mbin_mid, dMstar_photo - sig_dMstar_photo, dMstar_photo + sig_dMstar_photo, 
+            fc='C0', ec='none', alpha=0.5, label='Photometry only') 
+    sub.scatter(Mbin_mid, dMstar_photo, c='C0', s=2) 
+    sub.plot(Mbin_mid, dMstar_photo, c='C0') 
+    sub.fill_between(Mbin_mid, dMstar_specphoto - sig_dMstar_specphoto, dMstar_specphoto + sig_dMstar_specphoto, 
+            fc='C1', ec='none', alpha=0.5, label='Photometry+Spectroscopy') 
+    sub.scatter(Mbin_mid, dMstar_specphoto, c='C1', s=1) 
+    sub.plot(Mbin_mid, dMstar_specphoto, c='C1') 
+    #sub.scatter(Mstar_input, (Mstar_inf_photo[:,2]-Mstar_input), c='C0')
+    #sub.scatter(Mstar_input, (Mstar_inf_specphoto[:,2]-Mstar_input), c='C1')
+    sub.plot([9., 12.], [9., 12.], c='k', ls='--') 
+    sub.set_xlabel(r'$\log(~M^{\rm true}_{\rm tot}~[M_\odot]~)$', fontsize=25)
+    sub.set_xlim(9., 12.) 
+    sub.set_ylabel(r'$\log(~\widehat{M}_{\rm tot} / M^{\rm true}_{\rm tot}~)$', fontsize=25)
+    sub.set_ylim(-1., 1.) 
+    sub.legend(loc='upper right', fontsize=20, handletextpad=0.2) 
+    
+    _ffig = os.path.join(dir_fig, 'photo_vs_specphoto.%s.vanilla.noise_%s_%s.png' % (method, noise_photo, noise_specphoto)) 
+    fig.savefig(_ffig, bbox_inches='tight') 
+    fig.savefig(UT.fig_tex(_ffig, pdf=True), bbox_inches='tight') 
+    return None 
+
 def Fbestfit_spec(igal, noise='none', method='ifsps'): 
     ''' file name of best-fit of spectra of spectral_challenge galaxy #igal 
 
@@ -271,7 +439,7 @@ def Fbestfit_spec(igal, noise='none', method='ifsps'):
     return f_bf
 
 
-def Fbestfit_photo(igal, noise='none', dust=False, method='ifsps'): 
+def Fbestfit_photo(igal, noise='none', method='ifsps'): 
     ''' file name of best-fit of photometry of spectral_challenge galaxy #igal
 
     :param igal: 
@@ -287,13 +455,12 @@ def Fbestfit_photo(igal, noise='none', dust=False, method='ifsps'):
     :param method: 
         fitting method. (default: ifsps)
     '''
-    if dust: 
-        model = 'vanilla'
-    else: 
-        model = 'dustless_vanilla'
+    model = 'vanilla'
 
-    f_bf = os.path.join(UT.lgal_dir(), 'spectral_challenge', method, 
-            'photo.noise_%s.dust_%s.%s.%i.hdf5' % (noise, ['no', 'yes'][dust], model, igal))
+    if method == 'ifsps': 
+        f_bf = os.path.join(UT.dat_dir(), 'mini_mocha', 'ifsps', 'lgal.photo.noise_%s.%s.%i.hdf5' % (noise, model, igal))
+    elif method == 'pfirefly': 
+        f_bf = os.path.join(UT.dat_dir(), 'mini_mocha', 'pff', 'lgal.photo.noise_%s.%s.%i.hdf5' % (noise, model, igal))
     return f_bf
 
 
@@ -324,6 +491,8 @@ if __name__=="__main__":
     #mock_challenge_photo(noise='legacy', dust=False, method='ifsps')
     #mock_challenge_photo(noise='legacy', dust=True, method='ifsps')
 
-    mini_mocha_spec(noise='bgs0', method='ifsps')
-    mini_mocha_spec(noise='bgs0', method='pfirefly')
+    #mini_mocha_spec(noise='bgs0', method='ifsps')
+    #mini_mocha_spec(noise='bgs0', method='pfirefly')
+    #mini_mocha_photo(noise='legacy', method='ifsps')
     #mini_mocha_specphoto(noise='bgs0_legacy', method='ifsps')
+    photo_vs_specphoto(noise_photo='legacy', noise_specphoto='bgs0_legacy', method='ifsps')
